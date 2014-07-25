6554851314e7eb7ec86bf167b0909b0c83caa58d
Zachariah Moreno <xachmoreno@gmail.com>
2014-07-25 16:18:19 -0700

    renamed app to zm

--- bower_components/angular/angular.js
+++ bower_components/angular/angular.js
@@ -5077,8 +5077,8 @@ function $CacheFactoryProvider() {
  * Adding via the $templateCache service:
  *
  * ```js
- * var myApp = angular.module('myApp', []);
- * myApp.run(function($templateCache) {
+ * var zm = angular.module('zm', []);
+ * zm.run(function($templateCache) {
  *   $templateCache.put('templateId.html', 'This is the content of the template');
  * });
  * ```
@@ -13149,15 +13149,15 @@ function adjustMatchers(matchers) {
  *
  * **Example**:  Consider the following case. <a name="example"></a>
  *
- * - your app is hosted at url `http://myapp.example.com/`
+ * - your app is hosted at url `http://zm.example.com/`
  * - but some of your templates are hosted on other domains you control such as
  *   `http://srv01.assets.example.com/`,  `http://srv02.assets.example.com/`, etc.
- * - and you have an open redirect at `http://myapp.example.com/clickThru?...`.
+ * - and you have an open redirect at `http://zm.example.com/clickThru?...`.
  *
  * Here is what a secure configuration for this scenario might look like:
  *
  * ```
- *  angular.module('myApp', []).config(function($sceDelegateProvider) {
+ *  angular.module('zm', []).config(function($sceDelegateProvider) {
  *    $sceDelegateProvider.resourceUrlWhitelist([
  *      // Allow same origin resource loads.
  *      'self',
@@ -13167,7 +13167,7 @@ function adjustMatchers(matchers) {
  *
  *    // The blacklist overrides the whitelist so the open redirect here is blocked.
  *    $sceDelegateProvider.resourceUrlBlacklist([
- *      'http://myapp.example.com/clickThru**'
+ *      'http://zm.example.com/clickThru**'
  *    ]);
  *  });
  * ```
@@ -13622,7 +13622,7 @@ function $SceDelegateProvider() {
  *
  * <example module="mySceApp" deps="angular-sanitize.js">
  * <file name="index.html">
- *   <div ng-controller="myAppController as myCtrl">
+ *   <div ng-controller="zmController as myCtrl">
  *     <i ng-bind-html="myCtrl.explicitlyTrustedHtml" id="explicitlyTrustedHtml"></i><br><br>
  *     <b>User comments</b><br>
  *     By default, HTML that isn't explicitly trusted (e.g. Alice's comment) is sanitized when
@@ -13641,7 +13641,7 @@ function $SceDelegateProvider() {
  * <file name="script.js">
  *   var mySceApp = angular.module('mySceApp', ['ngSanitize']);
  *
- *   mySceApp.controller("myAppController", function myAppController($http, $templateCache, $sce) {
+ *   mySceApp.controller("zmController", function zmController($http, $templateCache, $sce) {
  *     var self = this;
  *     $http.get("test_data.json", {cache: $templateCache}).success(function(userComments) {
  *       self.userComments = userComments;
@@ -13693,7 +13693,7 @@ function $SceDelegateProvider() {
  * That said, here's how you can completely disable SCE:
  *
  * ```
- * angular.module('myAppWithSceDisabledmyApp', []).config(function($sceProvider) {
+ * angular.module('zmWithSceDisabledzm', []).config(function($sceProvider) {
  *   // Completely disable SCE.  For demonstration purposes only!
  *   // Do not use in new projects.
  *   $sceProvider.enabled(false);
--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('zm', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'zm.filters',
+  'zm.services',
+  'zm.directives',
+  'zm.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});

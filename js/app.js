b1750472ef14dfc698fe6bc1a627192b56a71971
Zachariah Moreno <xachmoreno@gmail.com>
2014-07-25 16:21:54 -0700

    renamed views

--- js/app.js
+++ js/app.js
@@ -10,7 +10,15 @@ angular.module('zm', [
   'zm.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/journal', {
+  	templateUrl: 'partials/journal.html', 
+  	controller: 'MyCtrl1'
+  });
+
+  $routeProvider.when('/health', {
+  	templateUrl: 'partials/health.html', 
+  	controller: 'MyCtrl2'
+  });
+
+  $routeProvider.otherwise({redirectTo: '/journal'});
 }]);
